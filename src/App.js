092e1187ef5cf38fa6d64c74c08386229fe60895
hepei <55754680+hp-kiki@users.noreply.github.com>
2020-01-03 17:34:07 +0800

    路由设置，tabbar设置

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-// import React from 'react';
-import { Button } from 'antd-mobile';
-
-
-import React, { Component } from 'react'
-
+import React, { Component,Fragment } from 'react'
+import { HashRouter as Router,Route} from "react-router-dom"
+import Home from "./pages/home"
+import Info from "./pages/info"
+import List from "./pages/list"
+import My from "./pages/my"
+import Hklayout from "./components/HKlayout/HKlayout"
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Button>antd-mobile按钮</Button>
-      </div>
+      <Fragment>
+        <Router>
+          <Route exact path="/" render={(props)=><Hklayout><Home {...props}/></Hklayout>}></Route>
+          <Route exact path="/info" render={(props)=><Hklayout><Info {...props}/></Hklayout>}></Route>
+          <Route exact path="/list" render={(props)=><Hklayout><List {...props}/></Hklayout>}></Route>
+          <Route exact path="/my" render={(props)=><Hklayout><My {...props}/></Hklayout>}></Route>
+        </Router>
+      </Fragment>
     )
   }
 }
-
---
+++ src/components/HKlayout/HKlayout.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import { TabBar } from 'antd-mobile';
+import {withRouter} from "react-router-dom"
+
+ class HKlayout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'blueTab',
+    };
+  }
+
+
+  render() {
+    return (
+      <div style={{ position: 'fixed', height: '100%', width: '100%', top: 0 }}>
+        <TabBar
+          unselectedTintColor="#949494"
+          tintColor="rgb(33, 185, 122)"
+          barTintColor="white"
+        >
+          <TabBar.Item
+            title="首页"
+            key="Life"
+            icon={<i className="iconfont icon-ind"></i>}
+            selectedIcon={<i className="iconfont icon-ind"></i>
+            }
+            selected={this.props.match.path==='/'}
+            onPress={() => {
+             this.props.history.push('/')
+            }}
+          >
+           {this.props.children}
+          </TabBar.Item>
+          <TabBar.Item
+            icon={
+                <i className="iconfont icon-findHouse"></i>
+            }
+            selectedIcon={
+                <i className="iconfont icon-findHouse"></i>
+            }
+            title="找房"
+            key="Koubei"
+            selected={this.props.match.path==='/info'}
+            onPress={() => {
+                this.props.history.push('/info')
+            }}
+          >
+               {this.props.children}
+          </TabBar.Item>
+          <TabBar.Item
+            icon={
+                <i className="iconfont icon-infom"></i>
+            }
+            selectedIcon={
+                <i className="iconfont icon-infom"></i>
+            }
+            title="资讯"
+            key="Friend"
+            selected={this.props.match.path==='/list'}
+            onPress={() => {
+                this.props.history.push('/list')
+            }}
+          >
+               {this.props.children}
+          </TabBar.Item>
+          <TabBar.Item
+            icon={ <i className="iconfont icon-myinfo"></i>}
+            selectedIcon={ <i className="iconfont icon-myinfo"></i>}
+            title="我的"
+            key="my"
+            selected={this.props.match.path==='/my'}
+            onPress={() => {
+                this.props.history.push('/my')
+            }}
+          >
+             {this.props.children}
+          </TabBar.Item>
+        </TabBar>
+      </div>
+    );
+  }
+}
+
+
+export default withRouter(HKlayout)
