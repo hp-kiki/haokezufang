03225a1fab4c67bbe5782c030f267d7c79004587
hepei <55754680+hp-kiki@users.noreply.github.com>
2020-01-04 10:24:06 +0800

    注释说明

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
+import axios from "../../ulits/myaxios"
 
 export default class index extends Component {
+
+    //发送请求
+    componentDidMount(){
+        axios.get("/home/swiper").then(res=>{
+            console.log(res)
+        })
+    }
+
     render() {
         return (
             <div>
